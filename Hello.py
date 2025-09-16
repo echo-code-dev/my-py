f7c994a54cb0ce54d4c49d1b1d47d67d7f41d212
Okoro Emmanuel <echoemmanuel2@gmail.com>
2025-09-16 03:19:21 +0100

    Version 3

--- Hello.py
+++ Hello.py
@@ -1,3 +1,8 @@
 print("hello python")
 print("just trying out git")
-print("this is version2")
+for value in range(0, 10, 2):
+  if value == 8:
+    print(value)
+  else:
+    print(value, end=', ')
+print("this is version 3")
